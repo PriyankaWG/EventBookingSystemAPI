56cd58e9a30c17e7b4fae92a313669f8222f5163
PriyankaWG <125970122+PriyankaWG@users.noreply.github.com>
2023-04-05 14:31:57 +0530

    Delete Event Booking System directory

--- Event Booking System/Classes/venue.py
+++
@@ -1,7 +0,0 @@
-class Venue:
-
-    def __init__(self,address,price,nbr_tickets):
-        self.address=address
-        self.price=price
-        self.nbr_tickets=nbr_tickets
-        
--- Event Booking System/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.role import RolesModel
-from models.accounts import AccountsModel
-from models.performer import PerformersModel
-from models.events import EventsModel
-from models.venue import VenuesModel
-from models.bookings import BookingsModel
-from models.tickets import TicketsModel
